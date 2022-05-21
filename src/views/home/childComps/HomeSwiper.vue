a6f31f7d0839b6144c28297fd165ce95297666dd
yellowsoul <517878555@qq.com>
2022-05-22 00:09:43 +0800

    P157 16-(掌握)首页开发-推荐信息的展示

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,38 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners" :key="item.id">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+// import Swiper from 'components/common/swiper/Swiper'
+// import SwiperItem from 'components/common/swiper/SwiperItem'
+import {Swiper, SwiperItem} from 'components/common/swiper'
+export default {
+  nmae:"HomeSwiper",
+  components:{
+    Swiper,
+    SwiperItem
+  },
+  props:{
+    banners:{
+      type:Array,
+      default:()=>[]
+      // default(){
+      //   return []
+      // }
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style scoped>
+</style>
