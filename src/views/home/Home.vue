1946467a699808cb2c958724b31cc3a126f351f9
yellowsoul <517878555@qq.com>
2022-05-22 18:37:20 +0800

    P165 07-(掌握)Better-Scroll的安装和使用

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,45 +3,173 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
-    <h2>首页</h2>
+    <feature-view />
+    <tab-control class="tab-control" 
+      :titles="['流行','新款','经典']" 
+      @tabClick="tabClick"/>
+    <goodsList :goods="showGoods"/>
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
 
-import {getHomeMultidata} from 'network/home'
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
+
+import {getHomeMultidata,getHomeGoods} from 'network/home'
 
 
 export default {
   name:'Home',
   components:{
-    NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
+    GoodsList
   },
   data(){
     return{
       banners:[],
-      recommends:[]
+      recommends:[],
+      goods:{
+        'pop':{page:0,list:[]},
+        'new':{page:0,list:[]},
+        'sell':{page:0,list:[]}
+      },
+      currentType:'pop'
+    }
+  },
+  computed:{
+    //传入的初始数据 ->处理为了看着清楚
+    showGoods(){
+      return this.goods[this.currentType].list
     }
   },
   created(){
     // 1.请求多个数据
-    getHomeMultidata().then(res => {
-      // console.log(res)
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    })
+    this.getHomeMultidata()
+
+    // 2.请求商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods:{
+    /**
+     * 事件监听相关的方法
+     */
+
+    tabClick(index) {
+      switch(index){
+        case 0:
+          this.currentType = 'pop'
+          break;
+        case 1:
+          this.currentType = 'new'
+          break;
+        case 2:
+          this.currentType = 'sell'
+          break;
+      }
+    },
+
+    /**
+     * 网络请求相关的方法
+     */
+    getHomeMultidata(){
+      getHomeMultidata().then(res => {
+        // console.log(res)
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+    
+    getHomeGoods(type){
+      const page = this.goods[type].page +1
+      getHomeGoods(type,page).then(res => {
+        // console.log(res)
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page = page
+      })
+    }
   }
 }
 
 </script>
 <style scoped>
+#home{
+  padding-top:44px;
+}
 .home-nav{
+  position: fixed;
+  top:0;
+  left:0;
+  right:0;
+  z-index: 9;
   background-color:var(--color-tint);
   color:#fff;
+}
+.tab-control{
+  position: sticky;
+  top:44px;
+  z-index: 9;
 }
 </style>
